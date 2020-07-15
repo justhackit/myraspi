d95face8914c604ae2123184989ceae3fc25dea5
aedapa <ajay.edapalapaty@nike.com>
2020-07-15 16:41:25 -0700

    beacong

--- python_scripts/beacon_scanv1.py
+++ python_scripts/beacon_scanv1.py
@@ -16,28 +16,38 @@ class ScanDelegate(DefaultDelegate):
 def convertRSSIStrToInt(rssi):
     return int(re.sub("[^0-9-]", "", rssi))
 
+def rssiChange(prev,curr):
+    return abs((abs(curRSSI) - abs(prevRSSI))) 
+
 if __name__ == "__main__":
-    prevRSSI=""
+    prevRSSI=0
     dataLogger = open("/home/pi/logs/beacon_rssi.log","w",buffering=0) 
     scanner = Scanner().withDelegate(ScanDelegate())
     while 1:
         try:
-            devices = scanner.scan(0.5)
+            devices = scanner.scan(1)
     #        print("Amount of Devices = "+str(len(devices)))
+            device=None
             for ii in devices:
-                #print("\t\t\tTesting "+ ii.addr)
                 if ii.addr.encode('ascii','ignore') == '18:04:ed:ab:5b:04':
-                    curRSSI = convertRSSIStrToInt(ii.rssi)
-                    if curRSSI != prevRSSI and abs((abs(curRSSI) - abs(prevRSSI))) > 2 :
-                        times=datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                        logStr="{0}:RSSI={1} dB".format(times,ii.rssi)
-                        print(logStr)
-                        dataLogger.write(logStr)
-                        dataLogger.flush()
-                    prevRSSI=curRSSI
-        except :
-            continue
-        finally:
-            dataLogger.close()
+                    device=ii
+                    print("Device Found")
+                    break
+
+            if device != None:
+                curRSSI = device.rssi
+                if curRSSI != prevRSSI and rssiChange(prevRSSI,curRSSI) > 0 :
+                    times=datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                    logStr="{0}|RSSI={1},Diff={2}".format(times,device.rssi,rssiChange(prevRSSI,curRSSI))
+                    print("\t"+logStr)
+                    dataLogger.write(logStr+"\n")
+                prevRSSI=curRSSI
+            else:
+                print("device not found")
+                dataLogger.write("device not found\n")
+        except Exception as e:
+            print(e)
+            dataLogger.write(e)
+    dataLogger.close()
 
 
